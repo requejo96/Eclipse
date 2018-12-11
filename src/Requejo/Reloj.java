6eb6318448c758de233d8c6fd40f1bbd99e4b9f3
Requejo <pablogr_1996@hotmail.es>
2018-12-11 12:10:24 +0100

    Punto2

--- src/Requejo/Reloj.java
+++ src/Requejo/Reloj.java
@@ -1,10 +1,44 @@
 package Requejo;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class Reloj {
 
 	private int Horas;
 	private int Minutos;
 	private int Segundos;
+	
+	
+	public void write() {
+		
+		// Copiar ficheros
+		File origen = new File("origen.txt");
+		File destino = new File("destino.txt");
+
+		try {
+		  InputStream in = new FileInputStream(origen);
+		  OutputStream out = new FileOutputStream(destino);
+						
+		  byte[] buf = new byte[1024];
+		  int len;
+
+		  while ((len = in.read(buf)) > 0) {
+		    out.write(buf, 0, len);
+		  }
+				
+		  in.close();
+		  out.close();
+		} catch (IOException ioe){
+		  ioe.printStackTrace();
+		}
+
+		
+	}
 	
 	
 	public Reloj(int horas, int minutos, int segundos) {
